7e745e026dc96a19dc0d9fb05d7591a1358fc04d
JAG111111 <jgoo2901@gmail.com>
2024-08-01 11:38:51 -0500

    commit11

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 let randomnumber = Math.floor(Math.random() * 100) + 1;
 
-function makeGuess() {
-    const userGuess = document.getElementById('guess-input').value;
-    const message = document.getElementById('message');
-    if(userGuess ==randomnumber){
-        message.textContent = "Good Job!";
-        message.style.color = 'blue';
-        resetGame();
-    }else if(userGuess > randomnumber) {
-        message.textContent = "Too High! Bad Job";
-        message.style.color = 'red';
-    }else if (userGuess < randomnumber) {
-        message.textContent = "Too Low! Bad Job";
-        message.style.color = 'red';    
-    }
+const gusses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-}
-function resetGame(){
-    randomnumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById('guess-input').value ='';
-}
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
